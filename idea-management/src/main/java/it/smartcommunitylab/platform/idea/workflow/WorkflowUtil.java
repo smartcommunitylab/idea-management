3264fa931977db940e2a3e8a8bb220dcf768fa0c
mirkoperillo <mirko.perillo@gmail.com>
2015-07-16 16:05:11 +0200

    removed useless context vars

--- idea-management/src/main/java/it/smartcommunitylab/platform/idea/workflow/WorkflowUtil.java
+++ idea-management/src/main/java/it/smartcommunitylab/platform/idea/workflow/WorkflowUtil.java
@@ -2,11 +2,7 @@ package it.smartcommunitylab.platform.idea.workflow;
 
 import java.util.HashMap;
 
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
-import com.liferay.portal.model.Role;
-import com.liferay.portal.service.RoleLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
 
 public class WorkflowUtil {
@@ -18,14 +14,6 @@ public class WorkflowUtil {
 				"no-reply@local.futuratrento.it");
 		workflowContext.put(WorkflowConstants.CONTEXT_NOTIFICATION_SENDER_NAME,
 				"FuturaTrento");
-
-		try {
-			Role r = RoleLocalServiceUtil.getRole(ctx.getCompanyId(),
-					"Blacklisted");
-			workflowContext.put("BLACK_ROLE_ID", String.valueOf(r.getRoleId()));
-		} catch (PortalException | SystemException e) {
-			e.printStackTrace();
-		}
 
 		ctx.setAttribute("workflowContext", workflowContext);
 
