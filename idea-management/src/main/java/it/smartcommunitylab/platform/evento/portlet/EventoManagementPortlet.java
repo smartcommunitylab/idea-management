d4c7ae732c7fc65b3aede3d7730e5a06d1f8721e
micnori <micnori@yahoo.com>
2015-07-16 16:38:26 +0200

    add first version eventi portlet

---
+++ idea-management/src/main/java/it/smartcommunitylab/platform/evento/portlet/EventoManagementPortlet.java
@@ -0,0 +1,325 @@
+package it.smartcommunitylab.platform.evento.portlet;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.portlet.PortletException;
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import com.liferay.calendar.model.CalendarBooking;
+import com.liferay.calendar.model.CalendarResource;
+import com.liferay.calendar.service.CalendarBookingLocalServiceUtil;
+import com.liferay.portal.kernel.dao.orm.Criterion;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.OrderFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
+import com.liferay.portal.kernel.dao.search.ResultRow;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.asset.model.AssetEntry;
+import com.liferay.portlet.asset.service.AssetEntryLocalServiceUtil;
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class EventoManagementPortlet
+ */
+public class EventoManagementPortlet extends MVCPortlet {
+	private static SimpleDateFormat sdfQueryDate = new SimpleDateFormat("yyyy-MM-dd");
+	
+	@Override
+	public void doView(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
+		ThemeDisplay themeDisplay = (ThemeDisplay) renderRequest.getAttribute(WebKeys.THEME_DISPLAY);		Date queryDate = null;
+		//String languageId = themeDisplay.getLanguageId();
+		Locale locale = themeDisplay.getLocale();
+		
+		String queryDateString = ParamUtil.getString(renderRequest, "date");
+		if(isNull(queryDateString)) {
+			queryDate = new Date();
+		} else {
+			try {
+				queryDate = sdfQueryDate.parse(queryDateString);
+			} catch (ParseException e) {
+				queryDate = new Date();
+			}
+		}
+		//next date
+		Calendar nextDateCalendar = GregorianCalendar.getInstance();
+		nextDateCalendar.setTime(queryDate);
+		nextDateCalendar.set(Calendar.HOUR_OF_DAY, 0); // ! clear would not reset the hour of day !
+		nextDateCalendar.clear(Calendar.MINUTE);
+		nextDateCalendar.clear(Calendar.SECOND);
+		nextDateCalendar.clear(Calendar.MILLISECOND);
+		nextDateCalendar.set(Calendar.DAY_OF_MONTH, 1);
+		nextDateCalendar.add(Calendar.MONTH, 1);
+		String nextDate = sdfQueryDate.format(nextDateCalendar.getTime());
+		System.out.println("nextDate:" + nextDate);
+		renderRequest.setAttribute("nextDate", nextDate);
+		//previous date
+		Calendar prevDateCalendar = GregorianCalendar.getInstance();
+		prevDateCalendar.setTime(queryDate);
+		prevDateCalendar.set(Calendar.HOUR_OF_DAY, 0); // ! clear would not reset the hour of day !
+		prevDateCalendar.clear(Calendar.MINUTE);
+		prevDateCalendar.clear(Calendar.SECOND);
+		prevDateCalendar.clear(Calendar.MILLISECOND);
+		prevDateCalendar.set(Calendar.DAY_OF_MONTH, 1);
+		prevDateCalendar.add(Calendar.MONTH, -1);
+		String prevDate = sdfQueryDate.format(prevDateCalendar.getTime());
+		System.out.println("prevDate:" + prevDate);
+		renderRequest.setAttribute("prevDate", prevDate);
+		
+		//title data
+		SimpleDateFormat sdfTitleDate = new SimpleDateFormat("MMMM yy", locale);
+		String titleData = sdfTitleDate.format(queryDate);
+		renderRequest.setAttribute("titleData", titleData);
+		
+		long categoryId = ParamUtil.getLong(renderRequest, "categoryId");
+		long groupId = ParamUtil.getLong(renderRequest, "groupId");
+		System.out.println("groupId param:" + groupId);
+//		if(groupId == 0) {
+//			groupId = themeDisplay.getScopeGroupId();
+//			System.out.println("groupId:" + groupId);
+//		}
+		List<CalendarBooking> eventList = new ArrayList<CalendarBooking>();
+		if(groupId > 0) {
+			try {
+				eventList = getEventsByGroupId(queryDate, groupId);
+			} catch (SystemException e) {
+				e.printStackTrace();
+			}
+		} else if(categoryId > 0) {
+			try {
+				eventList = getEventsByCategoryId(queryDate, categoryId);
+			} catch (SystemException e) {
+				e.printStackTrace();
+			} catch (PortalException e) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				eventList = getEventsByDate(queryDate);
+			} catch (SystemException e) {
+				e.printStackTrace();
+			}
+		}
+//		getSearchIteratorData(renderRequest, renderResponse, eventList);
+		
+		//events as list of map attribute
+		SimpleDateFormat sdf = new SimpleDateFormat("dd MMMM yyyy", locale);
+		List<Map<String, String>> eventMapList = new ArrayList<Map<String, String>>();
+		for(CalendarBooking event : eventList) {
+			Map<String, String> eventMap = new HashMap<String, String>();
+			eventMap.put("title", event.getTitle(locale));
+			eventMap.put("description", event.getDescription(locale));
+			eventMap.put("startDate", sdf.format(new Date(event.getStartTime())));
+			eventMapList.add(eventMap);
+		}
+		renderRequest.setAttribute("eventList", eventMapList);
+		super.doView(renderRequest, renderResponse);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<CalendarBooking> getEventsByDate(Date queryDate) throws SystemException {
+		List<CalendarBooking> eventList;
+		DynamicQuery dynamicQuery = CalendarBookingLocalServiceUtil.dynamicQuery();
+		Criterion criterionStatus = RestrictionsFactoryUtil.eq("status", WorkflowConstants.STATUS_APPROVED);
+		Long[] range = getTimeInterval(queryDate);
+		Criterion criterionTime = RestrictionsFactoryUtil.between("startTime", range[0], range[1]);
+		dynamicQuery.add(RestrictionsFactoryUtil.and(criterionTime, criterionStatus));
+		dynamicQuery.addOrder(OrderFactoryUtil.asc("startTime"));
+		dynamicQuery.setLimit(0, 5);
+		eventList = (List<CalendarBooking>)CalendarBookingLocalServiceUtil.dynamicQuery(dynamicQuery);
+		System.out.println("event count:" + eventList.size());
+		return eventList;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<CalendarBooking> getEventsByCategoryId(Date queryDate, long categoryId) throws SystemException, PortalException {
+		List<CalendarBooking> eventList = new ArrayList<CalendarBooking>();
+		DynamicQuery dynamicQuery = CalendarBookingLocalServiceUtil.dynamicQuery();
+		Criterion criterionStatus = RestrictionsFactoryUtil.eq("status", WorkflowConstants.STATUS_APPROVED);
+		//Criterion criterionIds = RestrictionsFactoryUtil.in("calendarBookingId", ids);
+		Long[] range = getTimeInterval(queryDate);
+		Criterion criterionTime = RestrictionsFactoryUtil.between("startTime", range[0], range[1]);
+		dynamicQuery.add(RestrictionsFactoryUtil.and(criterionTime, criterionStatus));
+		dynamicQuery.addOrder(OrderFactoryUtil.asc("startTime"));
+		dynamicQuery.setLimit(0, 100);
+		List<CalendarBooking> queryEventList = (List<CalendarBooking>)CalendarBookingLocalServiceUtil.dynamicQuery(dynamicQuery);
+		
+		int count = 0;
+		for(CalendarBooking event : queryEventList) {
+			AssetEntry entry = AssetEntryLocalServiceUtil.getEntry(CalendarBooking.class.getName(), event.getCalendarBookingId());
+			long[] categoryIds = entry.getCategoryIds();
+			if(ArrayUtil.contains(categoryIds, categoryId)) {
+				eventList.add(event);
+				count++;
+			}
+			if(count >= 5) {
+				break;
+			}
+		}
+		System.out.println("event count:" + eventList.size());
+		return eventList;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<CalendarBooking> getEventsByGroupId(Date queryDate, long groupId) throws SystemException {
+		List<CalendarBooking> eventList;
+		DynamicQuery dynamicQuery = CalendarBookingLocalServiceUtil.dynamicQuery();
+		Criterion criterionGroup = RestrictionsFactoryUtil.eq("groupId", groupId);
+		Criterion criterionStatus = RestrictionsFactoryUtil.eq("status", WorkflowConstants.STATUS_APPROVED);
+		Long[] range = getTimeInterval(queryDate);
+		Criterion criterionTime = RestrictionsFactoryUtil.between("startTime", range[0], range[1]);
+		dynamicQuery.add(RestrictionsFactoryUtil.and(criterionTime, RestrictionsFactoryUtil.and(criterionGroup, criterionStatus)));
+		dynamicQuery.addOrder(OrderFactoryUtil.asc("startTime"));
+		dynamicQuery.setLimit(0, 5);
+		eventList = (List<CalendarBooking>)CalendarBookingLocalServiceUtil.dynamicQuery(dynamicQuery);
+		System.out.println("event count:" + eventList.size());
+		return eventList;
+	}
+
+	private static Long[] getTimeInterval(Date date) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy mm:ss");
+		
+		Calendar calendarStart = GregorianCalendar.getInstance();
+		calendarStart.setTime(date);
+		calendarStart.set(Calendar.HOUR_OF_DAY, 0); // ! clear would not reset the hour of day !
+		calendarStart.clear(Calendar.MINUTE);
+		calendarStart.clear(Calendar.SECOND);
+		calendarStart.clear(Calendar.MILLISECOND);
+		calendarStart.set(Calendar.DAY_OF_MONTH, 1);
+		System.out.println(sdf.format(calendarStart.getTime()));
+		
+		Calendar calendarEnd = GregorianCalendar.getInstance();
+		calendarEnd.setTime(date);
+		calendarEnd.set(Calendar.HOUR_OF_DAY, 23); // ! clear would not reset the hour of day !
+		calendarEnd.set(Calendar.MINUTE, 59);
+		calendarEnd.set(Calendar.SECOND, 59);
+		calendarEnd.clear(Calendar.MILLISECOND);
+		calendarEnd.set(Calendar.DAY_OF_MONTH, calendarStart.getActualMaximum(Calendar.DAY_OF_MONTH));
+		System.out.println(sdf.format(calendarEnd.getTime()));
+		
+		Long[] result = new Long[2];
+		result[0] = calendarStart.getTimeInMillis();
+		result[1] = calendarEnd.getTimeInMillis();
+		return result;
+	}
+	
+	private boolean isNull(String string) {
+		if((string == null) || (string.isEmpty()))
+			return true;
+		return false;
+	}
+
+	@SuppressWarnings("unused")
+	private void getSearchIteratorData(RenderRequest renderRequest, RenderResponse renderResponse,
+			List<CalendarBooking> eventList) {
+		//search-iterator tag
+		List<String> headersEvent = new ArrayList<String>();
+		headersEvent.add("title");
+		headersEvent.add("description");
+		headersEvent.add("status");
+		headersEvent.add("calendar-name");
+		headersEvent.add("calendar-id");
+		headersEvent.add("calendar-resource-id");
+		headersEvent.add("start-date");
+		headersEvent.add("end-date");
+		headersEvent.add("group-id");
+		headersEvent.add("user-name");
+		PortletURL portletURL = renderResponse.createRenderURL();
+		SearchContainer<CalendarBooking> searchEvents = new SearchContainer<CalendarBooking>(renderRequest, null, null,
+				SearchContainer.DEFAULT_CUR_PARAM, 5, portletURL, headersEvent, "Empty resultset");
+		searchEvents.setTotal(eventList.size());
+		
+		List<String> headersResource = new ArrayList<String>();
+		headersResource.add("resource-id");
+		headersResource.add("class-name-id");
+		headersResource.add("class-name");
+		headersResource.add("class-uuid");
+		headersResource.add("code");
+		headersResource.add("name");
+		headersResource.add("description");
+		SearchContainer<CalendarResource> searchResources = new SearchContainer<CalendarResource>(renderRequest, null, null,
+				SearchContainer.DEFAULT_CUR_PARAM, 5, portletURL, headersResource, "Empty resultset");
+		searchResources.setTotal(eventList.size());
+
+		for (int i = searchEvents.getStart(); i < searchEvents.getEnd() && i < searchEvents.getTotal(); i++) {
+			CalendarBooking event = eventList.get(i);
+			try {
+				ResultRow rowEvent = new ResultRow(event, event.getCalendarBookingId(), i, false);
+				rowEvent.addText(event.getTitle("en_US"));
+				rowEvent.addText(event.getDescription("en_US"));
+				rowEvent.addText(Integer.toString(event.getStatus()));
+				rowEvent.addText(event.getCalendar().getName());
+				rowEvent.addText(Long.toString(event.getCalendar().getCalendarId()));
+				rowEvent.addText(Long.toString(event.getCalendar().getCalendarResourceId()));
+				rowEvent.addDate(new Date(event.getStartTime()));
+				rowEvent.addDate(new Date(event.getEndTime()));
+				rowEvent.addText(Long.toString(event.getGroupId()));
+				rowEvent.addText(event.getUserName());
+				searchEvents.getResultRows().add(rowEvent);
+				
+				CalendarResource resource = event.getCalendarResource();
+				ResultRow rowResource = new ResultRow(resource, resource.getCalendarResourceId(), i, false);
+				rowResource.addText(Long.toString(resource.getCalendarResourceId()));
+				rowResource.addText(Long.toString(resource.getClassNameId()));
+				rowResource.addText(resource.getClassName());
+				rowResource.addText(resource.getClassUuid());
+				rowResource.addText(resource.getCode());
+				rowResource.addText(resource.getName("en_US"));
+				rowResource.addText(resource.getDescription("en_US"));
+				searchResources.getResultRows().add(rowResource);
+			} catch (PortalException e) {
+				e.printStackTrace();
+			} catch (SystemException e) {
+				e.printStackTrace();
+			}
+		}
+		renderRequest.setAttribute("searchEvents", searchEvents);
+		renderRequest.setAttribute("searchResources", searchResources);
+	}
+	
+	public static void main(String[] args) {
+		Date actulaDate = new Date();
+		EventoManagementPortlet.getTimeInterval(actulaDate);
+		//next date
+		Calendar nextDateCalendar = GregorianCalendar.getInstance();
+		nextDateCalendar.setTime(actulaDate);
+		nextDateCalendar.set(Calendar.HOUR_OF_DAY, 0); // ! clear would not reset the hour of day !
+		nextDateCalendar.clear(Calendar.MINUTE);
+		nextDateCalendar.clear(Calendar.SECOND);
+		nextDateCalendar.clear(Calendar.MILLISECOND);
+		nextDateCalendar.set(Calendar.DAY_OF_MONTH, 1);
+		nextDateCalendar.add(Calendar.MONTH, 1);
+		String nextDate = sdfQueryDate.format(nextDateCalendar.getTime());
+		System.out.println("nextDate:" + nextDate);
+		//previous date
+		Calendar prevDateCalendar = GregorianCalendar.getInstance();
+		prevDateCalendar.setTime(actulaDate);
+		prevDateCalendar.set(Calendar.HOUR_OF_DAY, 0); // ! clear would not reset the hour of day !
+		prevDateCalendar.clear(Calendar.MINUTE);
+		prevDateCalendar.clear(Calendar.SECOND);
+		prevDateCalendar.clear(Calendar.MILLISECOND);
+		prevDateCalendar.set(Calendar.DAY_OF_MONTH, 1);
+		prevDateCalendar.add(Calendar.MONTH, -1);
+		String prevDate = sdfQueryDate.format(prevDateCalendar.getTime());
+		System.out.println("prevDate:" + prevDate);
+		
+	}
+}
---
+++ idea-management/src/main/webapp/html/eventomanagement/view.jsp
@@ -0,0 +1,55 @@
+<%@page import="java.util.Map"%>
+<%@page import="java.util.List"%>
+<%@page import="java.util.HashMap"%>
+<%@page import="java.text.SimpleDateFormat"%>
+<%@page import="com.liferay.calendar.model.CalendarBooking"%>
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet"%>
+<%@ taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui"%>
+<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<portlet:defineObjects />
+
+<%-- <jsp:useBean id="searchEvents" class="com.liferay.portal.kernel.dao.search.SearchContainer" scope="request"/>
+<jsp:useBean id="searchResources" class="com.liferay.portal.kernel.dao.search.SearchContainer" scope="request"/>
+ --%>
+<%-- <br/>
+<liferay-ui:search-iterator searchContainer="<%= searchEvents %>" />
+<br/>
+<liferay-ui:search-iterator searchContainer="<%= searchResources %>" />
+<br/> --%>
+
+<%
+	List<Map<String, String>> eventList = (List<Map<String, String>>) request.getAttribute("eventList");
+	String titleData = (String) request.getAttribute("titleData");
+	String prevDate = (String) request.getAttribute("prevDate");
+	String nextDate = (String) request.getAttribute("nextDate");
+%>
+
+<portlet:renderURL var="viewPrev">
+	<portlet:param name="date" value="<%= prevDate %>" />
+</portlet:renderURL>
+<portlet:renderURL var="viewNext">
+	<portlet:param name="date" value="<%= nextDate %>" />
+</portlet:renderURL>
+
+<div id="eventList" class="eventListClass">
+	<label class="main"><liferay-ui:message key="evento-eventi" /> <%=titleData%></label>
+	<input type="button" class="prevButton"  value="prevDate" 
+		onClick="location.href = '<%= viewPrev %>';" />
+	<input type="button" class="nextButton"  value="nextDate" 
+		onClick="location.href = '<%= viewNext %>';" />
+	<ul>
+		<%
+			for (Map<String, String> event : eventList) {
+		%>
+		<li>
+			<div class="startDate"><%=event.get("startDate")%></div>
+			<div class="title"><%=event.get("title")%></div>
+			<div class="description"><%=event.get("description")%></div>
+		</li>
+		<%
+			}
+		%>
+	</ul>
+</div>
