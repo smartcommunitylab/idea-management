06179956c33ebeb6876b2363fa02b5b6e904a6e0
kazhamiakin <kazhamiakin@gmail.com>
2018-02-27 14:53:30 +0100

    field format

--- idea-management/src/main/webapp/WEB-INF/sql/tables.sql
+++ idea-management/src/main/webapp/WEB-INF/sql/tables.sql
@@ -1,6 +1,6 @@
 create table IM_Call (
 	uuid_ VARCHAR(75) null,
-	title VARCHAR(75) null,
+	title TEXT null,
 	callId LONG not null primary key,
 	groupId LONG,
 	companyId LONG,
@@ -25,7 +25,7 @@ create table IM_Idea (
 	userName VARCHAR(75) null,
 	createDate DATE null,
 	modifiedDate DATE null,
-	title VARCHAR(75) null,
+	title TEXT null,
 	longDesc TEXT null,
 	shortDesc VARCHAR(75) null,
 	userGroupId LONG,
